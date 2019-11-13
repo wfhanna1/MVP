d9539c173f3473a52346594ffd2f7a6a29ddf965
Wasim Hanna <wfhanna1@gmail.com>
2019-11-13 18:45:57 -0500

    Updating the code to be EF code first

---
+++ Migrations/20191113234019_MultiPlayerMatchTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace sclask.Migrations
+{
+    public partial class MultiPlayerMatchTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MultiPlayerMatches",
+                columns: table => new
+                {
+                    MultiPlayerMatchId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    MatchId = table.Column<int>(nullable: false),
+                    PlayerId = table.Column<int>(nullable: false),
+                    IsWinner = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MultiPlayerMatches", x => x.MultiPlayerMatchId);
+                    table.ForeignKey(
+                        name: "FK_MultiPlayerMatches_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MultiPlayerMatches_MatchId",
+                table: "MultiPlayerMatches",
+                column: "MatchId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MultiPlayerMatches");
+        }
+    }
+}
