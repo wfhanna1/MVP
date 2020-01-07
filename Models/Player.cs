9e364f939fea66327fa59636704fc37133f90ff6
Wasim Hanna <wfhanna1@gmail.com>
2020-01-07 12:33:45 -0500

    Initial commit

--- Models/Player.cs
+++ Models/Player.cs
@@ -7,13 +7,16 @@ namespace sclask.Models
   public class Player
   {
     public int Id { get; set; }
+    
     [StringLength(255)]
     [Required]
     public string FullName { get; set; }
+    
     [EmailAddress]
     [Required]
     public string EmailAddress { get; set; }
-    [Url]
+    
+    
     public string ProfilePhoto { get; set; }
   }
 }
