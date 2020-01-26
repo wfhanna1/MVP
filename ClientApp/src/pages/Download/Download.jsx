8d5e8f0e4d71d12b98da7acf9676354f414da40b
Wasim Hanna <wfhanna1@gmail.com>
2020-01-26 14:30:57 -0500

    updated android url correctly

--- ClientApp/src/pages/Download/Download.jsx
+++ ClientApp/src/pages/Download/Download.jsx
@@ -7,7 +7,7 @@ export class Download extends Component {
         if(isIOS)
            return window.location.assign('https://install.appcenter.ms/orgs/itunes-uwoq-03/apps/mvpreactios/distribution_groups/beta%20testers');
         if(isAndroid)
-           return window.location.assign('install.appcenter.ms/orgs/itunes-uwoq-03/apps/mvpreactandroid/distribution_groups/beta%20testers');
+           return window.location.assign('https://install.appcenter.ms/orgs/itunes-uwoq-03/apps/mvpreactandroid/distribution_groups/beta%20testers');
         else
             return <h1>Please use a mobile device </h1>
     }
--- Migrations/SclaskDbContextModelSnapshot.cs
+++ Migrations/SclaskDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace sclask.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
-                .HasAnnotation("ProductVersion", "2.1.14-servicing-32113")
+                .HasAnnotation("ProductVersion", "2.1.1-rtm-30846")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             modelBuilder.Entity("sclask.Models.Game", b =>
